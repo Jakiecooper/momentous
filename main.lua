72fe66d69a9428a263a8f18aadf25c140522dbc2
Joey Trudeau <jtrudeau02@icloud.com>
2022-08-10 00:33:14 -0700

    adding player and loading level

--- main.lua
+++ main.lua
@@ -1,16 +1,25 @@
 local sti = require "sti"
+require("player")
 require "world"
 
 function love.load()
-  map = sti("testMap.lua")
-  setupWindow()
+  map = sti("assets/fixedMap.lua", {"box2d"})
+  World = love.physics.newWorld(0,0)
+  map:box2d_init(World)
+  map.layers.Collisons.visible = false
+  Player:load()
 end
 
 function love.update(dt)
-  map:update(dt)
+  World:update(dt)
+  Player:update(dt)
 end
 
 function love.draw()
-  map:draw(0, 0, xScale, yScale)
-  setScale()
+  map:draw(0, 0, 2, 2)
+  love.graphics.push()
+  love.graphics.scale(2,2)
+  Player:draw()
+  love.graphics.pop()
 end
+
